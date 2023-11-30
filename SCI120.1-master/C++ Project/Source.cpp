412120936cbe9be0d320cd826a2fa9b8a4943bb1
NINO THOMAS <nino.thomas96@stu.bmcc.cuny.edu>
2023-11-30 17:06:20 -0500

    k

--- SCI120.1-master/C++ Project/Source.cpp
+++ SCI120.1-master/C++ Project/Source.cpp
@@ -1,25 +1,17 @@
-//#include <iostream>
-//#include <fstream>
-//#include <vector>
-//#include <string>
-//using namespace std;
-//
-//struct xyPair { double x; double y; };
-//
-//double eval(const vector<double>& coefficients, double x);
-//vector<double> yDiff(const vector<double>& coefficients, const vector<xyPair>& data, double& error);
-//vector<double> gradientDescentStep(const vector<double>& coefficient, const vector<xyPair>& data, double& error);
-//vector<double> gradientDescent(const vector<xyPair>&& data, int degree);
-//
-//int main() {
-//	string inputFile;
-//	cout << "what is the name of your file?";
-//	cin >> inputFile;
-//
-//	ifstream inputFile(inputFile);
-//	if (!inputFile.is_open()) { cerr << "Error:Could not open the input file.\n"; return 1;}
-//
-//	vector<xyPair> data;
-//	double x, y;
-//
-//}
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Vector2D {
+	double x, y; 
+public: 
+	double get_x() { return x; };
+	double get_y() { return y; };
+	void set_x(double xx) { x = xx; };
+	void set_y(double yy) { y = yy; };
+	void setFromPolar(double rad, double ang, bool deg = true);
+};
+
+
+const double D2R = 3.14159 / 180.;
+void Vector2D::setFromPolar(double rad, double ang, bool deg) {if() };
